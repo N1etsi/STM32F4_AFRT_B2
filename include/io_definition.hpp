e09f70885870662c72a71e4648b5b18cee8e2e33
N1etsi <45513810+N1etsi@users.noreply.github.com>
2021-12-19 13:32:54 +0000

    setup

---
+++ include/io_definition.hpp
@@ -0,0 +1,15 @@
+//i2c addresses
+#define MPU_ADDR 0x68
+
+//pin declaration
+/*
+#define rx PA8
+#define escLF PA0
+#define escLB PA2
+#define escRF PA1
+#define escRB PA3
+#define redL PB14
+#define greenL PB12
+#define INTERRUPT_PIN PB5
+#define stL PC13
+*/
