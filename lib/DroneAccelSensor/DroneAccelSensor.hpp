d10c1b5665e0921aa1210b7462caabbd8efd0c13
N1etsi <45513810+N1etsi@users.noreply.github.com>
2022-01-26 15:02:11 +0000

    refactor 1

--- lib/DroneAccelSensor/DroneAccelSensor.hpp
+++
@@ -1,8 +0,0 @@
-#include <Adafruit_LSM9DS0.h>
-#include <Adafruit_Sensor.h>
-
-void displaySensorDetails(void);
-void beginLsm(void);
-void configureSensor(void);
-void getLsmEvents(sensors_event_t *accel, sensors_event_t *mag, sensors_event_t *gyro, sensors_event_t *temp);
-
